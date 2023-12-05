927560eeced9e708727d15f6da9a550ed8d5e6c6
ecorralro <ecorralr689@alumnos.imf.com>
2023-12-05 19:16:29 +0100

    bucle()

--- orm.py
+++ orm.py
@@ -1,7 +1,8 @@
 import random
 import tkinter as tk
 
-
+personas = []
+numeropersonas = 20
 
 # Creo objeto persona
 class Persona():
@@ -20,15 +21,28 @@ class Persona():
             self.posx + self.radio/2,
             self.posy + self.radio/2,
             fill = self.color)
+    def mueve(self):
+        lienzo.move(self.entidad,10,0)
 
 # Creo ventana
 raiz = tk.Tk()
 # Creo un lienzo en esa ventana
 lienzo = tk.Canvas(raiz,width=720,height=720)
 lienzo.pack()
+# Creo todo el número de personas y las dibujo
+for i in range(0,numeropersonas):
+    personas.append(Persona())
+for persona in personas:
+    persona.dibuja()
+# Creo un bucle para que esas personas se muevan
+def bucle():
+    for persona in personas:
+        persona.mueve()
+    raiz.after(100,bucle)
 
 persona = Persona()      
 Persona.dibuja(persona)
+bucle()
 
 
 raiz.mainloop()
