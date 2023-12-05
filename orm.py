5aefacd0a69d533dd2445a03dfcccea47e70a077
ecorralro <ecorralr689@alumnos.imf.com>
2023-12-05 19:03:06 +0100

    dibuja()

--- orm.py
+++ orm.py
@@ -1,20 +1,34 @@
 import random
 import tkinter as tk
 
-# Creo ventana
-raiz = tk.Tk()
-# Creo un lienzo en esa ventana
-lienzo = tk.Canvas(raiz,width=1024,height=1024)
-lienzo.pack()
 
-raiz.mainloop()
 
 # Creo objeto persona
 class Persona():
     def __init__(self):
-        self.posx = random.randint(0,1024)
-        self.posy = random.randint(0,1024)
+        self.posx = random.randint(0,720)
+        self.posy = random.randint(0,720)
         self.color = "red"
         self.radio = 25
         self.direccion = random.randint(0,360)
-        
+        self.entidad = ""
+# Dibujo en el lienzo
+    def dibuja(self):
+        self.entidad = lienzo.create_oval(
+            self.posx - self.radio/2,
+            self.posy - self.radio/2,
+            self.posx + self.radio/2,
+            self.posy + self.radio/2,
+            fill = self.color)
+
+# Creo ventana
+raiz = tk.Tk()
+# Creo un lienzo en esa ventana
+lienzo = tk.Canvas(raiz,width=720,height=720)
+lienzo.pack()
+
+persona = Persona()      
+Persona.dibuja(persona)
+
+
+raiz.mainloop()
